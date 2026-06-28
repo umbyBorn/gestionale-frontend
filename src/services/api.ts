e4dbdc8cad53b46042ccb1845c449b66811a4b94
umbyBorn <umberto.anguzza@gmail.com>
2026-06-28 13:49:20 +0200

    aggiornato URL backend a produzione

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://gestionale-sport-api.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
